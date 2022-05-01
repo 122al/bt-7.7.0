73e79821c56dd1c6f0e54ca01588660da5b1e132
122al <98227204+122al@users.noreply.github.com>
2022-05-02 02:41:18 +0800

    Update public.sh

--- install/public.sh
+++ install/public.sh
@@ -149,9 +149,9 @@ if [ -d "/www/server/phpmyadmin/pma" ];then
 	rm -rf /www/server/phpmyadmin/pma
 	EN_CHECK=$(cat /www/server/panel/config/config.json |grep English)
 	if [ "${EN_CHECK}" ];then
-		curl https://raw.githubusercontent.com/zhucaidan/btpanel-v7.7.0/main/install/update6_en.sh|bash
+		curl https://raw.githubusercontent.com/122al/bt-7.7.0/main/install/update6_en.sh|bash
 	else
-		curl https://raw.githubusercontent.com/zhucaidan/btpanel-v7.7.0/main/install/update6.sh|bash
+		curl https://raw.githubusercontent.com/122al/bt-7.7.0/main/install/update6.sh|bash
 	fi
 	echo > /www/server/panel/data/restart.pl
 fi
